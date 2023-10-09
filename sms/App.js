eb590d587af616c5097a8d0b8a0b844c36874517
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-09 10:26:55 -0300

    terminar formulario

--- sms/App.js
+++ sms/App.js
@@ -1,16 +1,19 @@
-import { StyleSheet, Text, View, Linking, Button, TouchableOpacity, Platform } from 'react-native';
+import { StyleSheet, Text, View, Linking, Button, TouchableOpacity, Platform, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
 
-  const llamarNumero = () => {
-    const phoneNumber = 1144719591;
+  const [texto, setTexto] = useState()
+
+  const llamarNumero = ({texto}) => {
+    const phoneNumber = {texto};
     Linking.openURL(`tel:${phoneNumber}`)
   }
 
   const mandarSMS = () => {
     const url = (Platform.OS === 'android')
-    ? 'sms:1144719591?body=HOLA'
-    : 'sms:1144719591'
+    ? 'sms:1160331228?body=HOLA'
+    : 'sms:1160331228'
     Linking.canOpenURL(url).then(supported => {
       if (!supported) {
         console.log('Unsupported url: ' + url)
@@ -20,11 +23,27 @@ export default function App() {
     }).catch(err => console.error('An error occurred', err))
   }
 
+  const mandarWhatsapp = () => {
+    const whatsappNo = 5491160331228
+    const whatsappMsg = "hola gay"
+    Linking.openURL(`whatsapp://send?phone=${whatsappNo}&text=${whatsappMsg}`);
+  }
+
   return (
     <View style={styles.container}>
+      
+      <Text>Ingrese el numero</Text>
+
+      <TextInput 
+      onChangeText={setTexto} 
+      value={texto} 
+      style={styles.input}
+      keyboardType="numeric"
+      />
+
       <Text>LLAMADAS TELEFONICAS:</Text>
 
-      <TouchableOpacity onPress={() => llamarNumero()}>
+      <TouchableOpacity onPress={() => llamarNumero({texto})}>
         <Text>Llamar</Text>
       </TouchableOpacity>
 
@@ -32,6 +51,12 @@ export default function App() {
 
       <TouchableOpacity onPress={() => mandarSMS()}>
         <Text>Textear</Text>
+      </TouchableOpacity>
+
+      <Text>MANDAR WHATSAPP:</Text>
+
+      <TouchableOpacity onPress={() => mandarWhatsapp()}>
+        <Text>Enviar</Text>
       </TouchableOpacity>
     </View>
   );
@@ -44,4 +69,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    backgroundColor: 'lightgray',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+  }
 });
